672e5a2ee27a6789f0a664c3b4abea9fda91f21c
chamsom <jaekimiyo@gmail.com>
2025-11-23 20:01:06 -0600

    added R script to create_package with devtools library

---
+++ artera_review/r_package/ex_package.R
@@ -0,0 +1,11 @@
+# Devtools supports package development
+library(devtools)
+
+# packageVersion("devtools")
+
+# Package to include:
+# 1. Functions: Case helpers for regular expressions (regexcite)
+# 2. Version control: Open development process
+# 3. Established workflows for install: Getting help and checking quality
+
+create_package("~/Documents/regexcite")
